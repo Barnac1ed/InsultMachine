0d7f250c8822b9607025de72c4cf8c8b8628d834
Barnac1ed <nolanhightower@outlook.com>
2021-09-30 18:45:35 +0000

    s

---
+++ index.js
@@ -0,0 +1,11 @@
+const insults = require("./insults/insults.js");
+const InsultMachine = () => {
+    return insults[Math.floor(Math.random() * insults.length)];
+}
+const compliments = require("./compliments/compliment.js");
+const ComplimentMachine = () => {
+    return compliments[Math.floor(Math.random() * compliments.length)];
+}
+module.exports.InsultMachine = InsultMachine;
+module.exports.ComplimentMachine = ComplimentMachine;
+
