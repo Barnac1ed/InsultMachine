9af5a016e2db22b890cc46f3e861db6e766b365f
Barnacled <65189536+Barnac1ed@users.noreply.github.com>
2021-09-30 11:05:00 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const insults = require("./insults/insults.js");
 const InsultMachine = () => {
     return insults[Math.floor(Math.random() * insults.length)];
 }
+const compliments = require("./compliments/compliment.js");
+const ComplimentMachine = () => {
+    return compliments[Math.floor(Math.random() * compliments.length)];
+}
 module.exports.InsultMachine = InsultMachine;
+module.exports.ComplimentMachine = ComplimentMachine;
+
